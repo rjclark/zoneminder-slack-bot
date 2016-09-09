73e89e5e20dbf3bed272771c69bd9d1402785671
Robert Clark <clark@exiter.com>
2016-09-08 20:22:52 -0400

    Reformatted and added configparser as an install requirement

--- setup.py
+++ setup.py
@@ -17,65 +17,66 @@
 from setuptools import setup
 
 setup(
-  name = 'zonebot',
-  version = '1.0',
+    name='zonebot',
+    version='1.0',
 
-  description = 'A Slack bot to interact with ZoneMinder',
+    description='A Slack bot to interact with ZoneMinder',
 
-  packages = ['zonebot'], # this must be the same as the name above
+    packages=['zonebot'],  # this must be the same as the name above
 
-  author = 'Robert Clark',
-  author_email = 'clark@exiter.com',
+    author='Robert Clark',
+    author_email='clark@exiter.com',
 
-  url = 'https://github.com/nogudnik/zoneminder-slack-bot', # use the URL to the github repo
-  license = 'Apache',
+    url='https://github.com/nogudnik/zoneminder-slack-bot',  # use the URL to the github repo
+    license='Apache',
 
-  keywords = ['slack', 'zoneminder'], # arbitrary keywords
+    keywords=['slack', 'zoneminder'],  # arbitrary keywords
 
-  include_package_data=True,
+    include_package_data=True,
 
-  classifiers = [
-    "Programming Language :: Python",
+    classifiers=[
+        "Programming Language :: Python",
 
-    # How mature is this project? Common values are
-    #   2 - Pre-Alpha
-    #   3 - Alpha
-    #   4 - Beta
-    #   5 - Production/Stable
-    'Development Status :: 2 - Pre-Alpha',
+        # How mature is this project? Common values are
+        #   2 - Pre-Alpha
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 2 - Pre-Alpha',
 
-    # Pick your license as you wish (should match "license" above)
-    'License :: OSI Approved :: Apache Software License',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: Apache Software License',
 
-    # Specify the Python versions you support here. In particular, ensure
-    # that you indicate whether you support Python 2, Python 3 or both.
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.2',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
 
-    "Topic :: Communications :: Chat",
-  ],
+        "Topic :: Communications :: Chat",
+    ],
 
-  # List run-time dependencies here.  These will be installed by pip when
-  # your project is installed. For an analysis of "install_requires" vs pip's
-  # requirements files see:
-  # https://packaging.python.org/en/latest/requirements.html
-  install_requires=[
-    'slackclient'
-  ],
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'slackclient',
+        'configparser'
+    ],
 
-  test_suite='nose2.collector.collector',
+    test_suite='nose2.collector.collector',
 
-  # To provide executable scripts, use entry points in preference to the
-  # "scripts" keyword. Entry points provide cross-platform support and allow
-  # pip to create the appropriate form of executable for the target platform.
-  entry_points={
-      'console_scripts': [
-          'zonebot=zonebot:main',
-      ],
-  },
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'zonebot=zonebot:main',
+        ],
+    },
 )
